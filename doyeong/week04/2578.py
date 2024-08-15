24e1fe28aad0db201576126ccee0840dca036f41
SorrowAddict <soantine789@naver.com>
2024-08-16 02:52:20 +0900

    [BOJ] 2578 - 빙고

--- doyeong/week04/2578.py
+++ doyeong/week04/2578.py
@@ -7,34 +7,44 @@ input = sys.stdin.readline
 arr = [list(map(int, input().split())) for _ in range(5)]
 
 def check_bingo():
-    count = 0
-
-    # 대각선 체크
-    if all(arr[i][i] == 0 for i in range(5)):
-        count += 1
-    if all(arr[i][4-i] == 0 for i in range(5)):
-        count += 1
-
-    # 가로 체크
+    temp = 0
+    # 대각선
     for i in range(5):
-        if all(arr[i][j] == 0 for j in range(5)):
-            count += 1
+        if arr[i][i] != 0:
+            break
+    else: temp += 1
+    
+    # 대각선
+    for i in range(5):
+        if arr[i][4-i] != 0:
+            break
+    else: temp += 1
+    
+    # 가로
+    for i in range(5):
+        for j in range(5):
+            if arr[i][j] != 0:
+                break
+        else: temp += 1
 
-    # 세로 체크
+    # 세로
     for j in range(5):
-        if all(arr[i][j] == 0 for i in range(5)):
-            count += 1
+        for i in range(5):
+            if arr[i][j] != 0:
+                break
+        else: temp += 1
+    return temp
 
-    return count
-
-# 빙고 수 확인 리스트 입력
+# 빙고 입력 받아오기
 lst = []
 for _ in range(5):
     lst.extend(map(int, input().split()))
 
+# 빙고 함수 돌리기
 cnt = 0
 for x in lst:
     cnt += 1
+    # 해당 번호를 X표(0으로) 바꿈
     for i in range(5):
         for j in range(5):
             if arr[i][j] == x:
@@ -42,8 +52,10 @@ for x in lst:
                 break
         else:
             continue
+        # 바꿨을 때 2중 for문 탈출
         break
-
+    
+    # 빙고 3회 이상이면 프린트하고 바로 정지
     if check_bingo() >= 3:
         print(cnt)
         break
