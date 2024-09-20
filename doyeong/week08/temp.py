5eb983215515e6b0e9f8c96d2e4d2dcd2ba1f503
Doyeong_Jung <sonatine789@naver.com>
2024-09-20 09:27:31 +0900

    [BOJ] 2920 - 음계

---
+++ doyeong/week08/temp.py
@@ -0,0 +1,10 @@
+# 2920. 음계
+
+lst = list(map(int, input().split()))
+ 
+if lst == sorted(lst):
+    print('ascending')
+elif lst == sorted(lst, reverse=True):
+    print('descending')
+else:
+    print('mixed')
