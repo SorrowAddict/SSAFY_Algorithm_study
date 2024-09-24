3f4d928978c83cbfbac1427fd7a0f256d9a1c9f3
zoe <ddu_buk_e@naver.com>
2024-09-24 20:14:11 +0900

    9주차 백준 큐문제, dfs 및 bfs 연습

---
+++ heee1/week09/BOJ2164_카드2.py
@@ -0,0 +1,17 @@
+from collections import deque
+N = int(input())
+
+q = deque()
+for i in range(1,N+1):
+    q.append(i)
+
+while len(q) != 1:
+    q.popleft()
+    q.append(q.popleft())
+
+print(*q)
+
+'''
+리뷰
+1. deque 처음 써봄
+'''
